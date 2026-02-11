9c415c5c1110effcb4b297c5490d9eed96253912
Helena Mougammadaly <helena.mougam@yahoo.fr>
2026-02-11 16:39:36 +0100

    created admin

---
+++ src/Controller/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[IsGranted('ROLE_ADMIN')]
+final class PostController extends AbstractController
+{
+    #[Route('/posts', name: 'app_post')]
+    public function index(): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'controller_name' => 'PostController',
+        ]);
+    }
+}
